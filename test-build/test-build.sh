b614c4c5c9df7013aca3961f71573aff50259279
kvzfly <247463413@qq.com>
2016-05-31 16:47:56 -0400

    This is a cicd teat 01

--- test-build/test-build.sh
+++ test-build/test-build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ${WORKSPACE}/src
-docker build -t 192.168.5.31:80/python-redis-demo:${BUILD_NUMBER} .
+docker build -t 192.168.5.36:5002/poc/python-redis-demo:${BUILD_NUMBER} .
 
-docker push 192.168.5.31:80/python-redis-demo:${BUILD_NUMBER}
+docker push 192.168.5.36:5002/poc/python-redis-demo:${BUILD_NUMBER}
 cd ${WORKSPACE}/test-build
 
 sed -i 's/\$\$BUILD_NUMBER\$\$/'${BUILD_NUMBER}'/g' docker-compose.yml
 sed -i 's/\$\$PORT_NUMBER\$\$/'`expr 5000 + ${BUILD_NUMBER}`'/g' docker-compose.yml
 chmod 777 ./rancher-compose
 
-./rancher-compose --access-key 67D80D1F783AFD302842 --secret-key HY27yD11kxAFMQRsxgLVHeWW7zax9SrNc2Yu4W15 -p python-redis-demo-build${BUILD_NUMBER} up -d
+./rancher-compose --access-key D5A257C31CC328001D40 --secret-key n6RtqzhVqt9Vhg4Wry4yaLN4TweEiozwav7WjBuR -p python-redis-demo-build${BUILD_NUMBER} up -d
